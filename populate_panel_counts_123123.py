8a6d2e5530deac5e55e0448aabc7b2349d238098
Matthew Lollar <mjlollar1@gmail.com>
2023-12-31 10:50:10 -0600

    Update and rename populate_panel_counts_122923.py to populate_panel_counts_123123.py

--- populate_panel_counts_123123.py
+++ populate_panel_counts_123123.py
@@ -1,4 +1,4 @@
-#### MJL 12/29/23
+#### MJL 12/31/23, added recombination 
 #### SCRIPT TO ADJUST PANEL FILES WITH ADDED OFFSPRING COUNT COLUMNS
 #### THIS SCRIPT IS A VARIANT OF "populate_snp_matrix.pl"
 #### USES TOTAL COUNTS IN ADDED COLUMNS INSTEAD OF 1/0 PRESENCE/ABSENCE
@@ -20,13 +20,13 @@ df_sync = pd.read_csv(args.s, sep='\t', header=None)
 ### Add headers for simple combine
 #A1 - allele1, A2 - allele2, 
 #P1A1 - Population1/allele1, etc.., P2A2 - population2/allele2
-df_panel.columns = ['chr', 'pos', 'A1', 'A2', 'P1A1', 'P1A2', 'P2A1', 'P2A2']
+df_panel.columns = ['chr', 'pos', 'A1', 'A2', 'P1A1', 'P1A2', 'P2A1', 'P2A2', 'recomb']
 df_sync.columns = ['chr', 'pos', 'refA', 'count']
 
 ### Merge Sync file and Panel files
 df_combined = pd.merge(df_panel, df_sync[['pos','count']], on='pos')
 df_combined[['A', 'T', 'C', 'G', 'countN', 'countDel']] = df_combined['count'].str.split(':', expand=True)
-df_combined.drop(df_combined.columns[[8,13,14]], axis=1, inplace=True) #drop unneeded columns(total counts, Indel and Del counts)
+df_combined.drop(df_combined.columns[[9,14,15]], axis=1, inplace=True) #drop unneeded columns(total counts, Indel and Del counts)
 
 ### Match offspring allele count with panel allele count
 def allele_match(row):
@@ -40,10 +40,10 @@ def allele_match(row):
 ### Generate new columns
 df_combined[["A1f", "A2f"]] = df_combined.apply(lambda row: allele_match(row), axis='columns', result_type='expand')
 
-### Drop columns with no allele count and remove intermediate columns for output
+### Drop rows with no allele counts in input sync (no information rows) and remove intermediate columns for output
 index_group = df_combined[(df_combined['A1f'] == 0) & (df_combined['A2f'] == 0)].index
 df_combined.drop(index_group, inplace=True)
-df_combined.drop(df_combined.columns[[8,9,10,11]], axis=1, inplace=True)
+df_combined.drop(df_combined.columns[[9,10,11,12]], axis=1, inplace=True)
 
 ### Print panel output
 outchrom = args.c + '.ahmm_in.panel'
