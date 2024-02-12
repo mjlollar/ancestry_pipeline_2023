9db15e4e5e794ba90d956f23790af879f220ad0e
Matthew Lollar <mjlollar1@gmail.com>
2024-02-12 11:32:11 -0600

    Update parental_syncs_to_panel.py

--- parental_syncs_to_panel.py
+++ parental_syncs_to_panel.py
@@ -1,4 +1,6 @@
 ### MJL 02/08/2024
+### Takes two parental sync files and generates a panel
+### Optional flag to adjust minimum read count in parental sync for consideration of a site
 
 import sys
 import argparse
@@ -13,29 +15,29 @@ parser.add_argument("--prc", help="Per Parent Minimum Total Read Count at Panel 
 parser.add_argument("--c", help="Chromosome Arm", required=True, type=str)
 args = parser.parse_args()
 
-#Load in data
+### Load in data
 print("Loading Syncs....")
 df_par1 = pd.read_csv(args.p1, sep='\t', header=None)
 df_par2 = pd.read_csv(args.p2, sep='\t', header=None)
-
 df_par1.columns = ['chr', 'pos', 'refA', 'p1_count']
 df_par2.columns = ['chr', 'pos', 'refA', 'p2_count']
 
+### Expand sync count columns
 df_par1[['p1A', 'p1T', 'p1C', 'p1G', 'p1countN', 'p1countDel']] = df_par1['p1_count'].str.split(':', expand=True)
 df_par2[['p2A', 'p2T', 'p2C', 'p2G', 'p2countN', 'p2countDel']] = df_par2['p2_count'].str.split(':', expand=True)
 
-#print("Merging overlapping read sites in parental syncs...")
+### Find overlapping read sites and marge syncs to common dataframe
+print("Merging overlapping read sites in parental syncs...")
 df_panel = pd.merge(df_par1, df_par2, on='pos')
 df_panel.drop(df_panel.columns[[2,3,8,9,10,11,12,17,18]], axis=1, inplace=True)
 
-#print("Dropping Reads with Threshold: >=" + str(args.prc))
 ### Drop rows with <# counts specified with input (per parent)
+print("Dropping Reads with Threshold: >=" + str(args.prc))
 df_panel = df_panel[(df_panel['p1A'].astype(int) + df_panel['p1T'].astype(int) + df_panel['p1C'].astype(int) + df_panel['p1G'].astype(int)).ge(args.prc)]
 df_panel = df_panel[(df_panel['p2A'].astype(int) + df_panel['p2T'].astype(int) + df_panel['p2C'].astype(int) + df_panel['p2G'].astype(int)).ge(args.prc)]
 
-print(df_panel)
 allele_dict = {'0':'A','1':'T','2':'C','3':'G'}
-# Function to determine Major and Minor Allele and add columns to panel
+### Function to determine Major and Minor Allele and add columns to panel
 def majorminor_grouped(row): 
 	A_count = int(row['p1A']) + int(row['p2A'])
 	T_count = int(row['p1T']) + int(row['p2T'])
@@ -59,7 +61,7 @@ def count_parental_alleles(row):
 	p1_count_index = np.argsort(par1_alleles)
 	p2_count_index = np.argsort(par2_alleles)
 
-	#Get allele ID of parental allele for A1 and A2 allele count
+	### Get allele ID of parental allele for A1 and A2 allele count
 	p1_A1 = allele_dict.get(str(p1_count_index[3]))
 	p2_A1 = allele_dict.get(str(p2_count_index[3]))
 	p1_A2 = allele_dict.get(str(p1_count_index[2]))
@@ -102,7 +104,7 @@ def count_parental_alleles(row):
 
 ### Get Major and Minor alleles at merged sites
 try:
-	#print("Determining Major/Minor Alleles and Counts...")
+	print("Determining Major/Minor Alleles and Counts...")
 	df_panel[["A1", "A2"]] = df_panel.apply(lambda row: majorminor_grouped(row), axis='columns', result_type='expand')
 except ValueError: #catch instances where empty dataframe
 	print("Emtpy Dataframe Detected. Are your read cutoffs too strict? (--prc flag)")
@@ -125,7 +127,7 @@ except ValueError: #catch instances where empty dataframe, unlikely here
 
 ### Drop rows with both Major/Minor counted (all 1's in added four columns)
 df_panel = df_panel[(df_panel['P1A1'].astype(int) + df_panel['P1A2'].astype(int) + df_panel['P2A1'].astype(int) + df_panel['P2A2'].astype(int)).ne(4)]
-### Drop unneeded row
+### Drop rows not needed in panel output
 df_panel.drop(df_panel.columns[[2,3,4,5,6,7,8,9]], axis=1, inplace=True)
 
 ### Add dummy recombination columns
